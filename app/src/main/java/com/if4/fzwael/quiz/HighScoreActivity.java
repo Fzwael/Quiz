e61a422fae8abafd2291c8cc18676200703e9dc7
wael fezai <fzwael@gmail.com>
2016-04-04 16:53:31 +0100

    test

--- app/src/main/java/com/if4/fzwael/quiz/HighScoreActivity.java
+++ app/src/main/java/com/if4/fzwael/quiz/HighScoreActivity.java
@@ -19,7 +19,6 @@ public class HighScoreActivity extends Activity {
         setContentView(R.layout.activity_high_score);
         TextView textResult = (TextView) findViewById(R.id.hstxt);
         SharedPreferences prefs = this.getSharedPreferences("highScore", Context.MODE_PRIVATE);
-        int score = prefs.getInt("key", 0); //0 is the default value
 
     }
 }
